2d4b986e08742627fa479b9b2e5ee149aa435caa
matiiv <matiiv.r.a@gmail.com>
2022-02-21 02:05:45 +0300

    add max_length to CharField

--- junior_hunter/models.py
+++ junior_hunter/models.py
@@ -4,24 +4,24 @@ from django.db import models
 
 
 class Company(models.Model):
-    name = models.CharField()
-    location = models.CharField()  # TODO сделать полем с выбором, возможно из другой модели
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)  # TODO сделать полем с выбором, возможно из другой модели
     logo = models.URLField(default='https://place-hold.it/100x60')
     description = models.TextField()
     employee_count = models.IntegerField()
 
 
 class Specialty(models.Model):
-    code = models.CharField()  # TODO сделать полем с выбором, возможно из другой модели
-    title = models.CharField()
+    code = models.CharField(max_length=100)  # TODO сделать полем с выбором, возможно из другой модели
+    title = models.CharField(max_length=100)
     picture = models.URLField(default='https://place-hold.it/100x60')
 
 
 class Vacancy(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=100)
     specialty = models.ForeignKey(Specialty, related_name="vacancies", on_delete=models.PROTECT)
     company = models.ManyToManyField(Company, related_name="vacancies")
-    skills = models.CharField()  # TODO попробовать ArrayField
+    skills = models.CharField(max_length=100)  # TODO попробовать ArrayField
     description = models.TextField()
     salary_min = models.FloatField()
     salary_max = models.FloatField()
