f8b7938d5b0dcf7ba693bc3b78d294d200922565
matiiv <matiiv.r.a@gmail.com>
2022-02-20 12:34:18 +0300

    replace hardcore message in view on template

--- junior_hunter/views.py
+++ junior_hunter/views.py
@@ -1,32 +1,30 @@
 from django.http import HttpResponse
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.shortcuts import render
 
 
 # – Главная  /
-class MainPageView(View):
-    def get(self, request):
-        return HttpResponse('Здесь будет главная страница')
+class MainPageView(TemplateView):
+    template_name = 'index.html'
 
 
 # – Одна вакансия /vacancies/22
-class VacancyView(View):
-    def get(self, request, vacancy_id):
-        return HttpResponse("Здесь будет карточка вакансии")
+class VacancyView(TemplateView):
+    template_name = 'vacancy.html'
 
 
 # – Вакансии по специализации /vacancies/cat/frontend
-class CategoryVacancyView(View):
-    def get(self, request, category):
-        return HttpResponse("Здесь будет карточка категории вакансий")
+class CategoryVacancyView(TemplateView):
+    template_name = 'vacancies.html'
 
 
 # – Все вакансии списком  /vacancies
-class AllVacanciesView(View):
-    def get(self, request):
-        return HttpResponse("Здесь будет страница со всеми вакансиями")
+class AllVacanciesView(TemplateView):
+    # TODO переделать шаблон тк этот похож на вакансии по категориям
+    template_name = 'vacancies.html'
 
 
 # – Карточка компании  /companies/345
-class CompanyView(View):
-    def get(self, request, company_id):
-        return HttpResponse("Здесь будет карточка компании")
+class CompanyView(TemplateView):
+    template_name = 'company.html'
