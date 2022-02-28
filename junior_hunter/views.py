2a0b48e50c52d8515320aa9069d98ddfc5361e54
matiiv <matiiv.r.a@gmail.com>
2022-02-28 05:40:01 +0300

    ref

--- junior_hunter/views.py
+++ junior_hunter/views.py
@@ -1,27 +1,28 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 # – Главная  /
 class MainPageView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'junior_hunter/index.html'
 
 
 # – Одна вакансия /vacancies/22
 class VacancyView(TemplateView):
-    template_name = 'vacancy.html'
+    template_name = 'junior_hunter/vacancy.html'
 
 
 # – Вакансии по специализации /vacancies/cat/frontend
 class CategoryVacancyView(TemplateView):
-    template_name = 'vacancies.html'
+    template_name = 'junior_hunter/vacancies.html'
 
 
 # – Все вакансии списком  /vacancies
 class AllVacanciesView(TemplateView):
     # TODO переделать шаблон тк этот похож на вакансии по категориям
-    template_name = 'vacancies.html'
+    template_name = 'junior_hunter/vacancies.html'
 
 
 # – Карточка компании  /companies/345
 class CompanyView(TemplateView):
-    template_name = 'company.html'
+    template_name = 'junior_hunter/company.html'
