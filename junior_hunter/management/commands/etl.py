b89af7686619fb81ea1d9a8a4ce386c2d3202764
matiiv <matiiv.r.a@gmail.com>
2022-03-07 19:24:38 +0300

    write etl process via ORM

--- junior_hunter/management/commands/etl.py
+++ junior_hunter/management/commands/etl.py
@@ -1,157 +1,143 @@
-"""
-python manage.py etl --path_to_sqlite='db.sqlite3' --path_to_config='ETL/config.yaml'
-"""
-import yaml
-from django.core.management.base import BaseCommand, CommandError
-# from polls.models import Question as Poll
-
 import logging
-import sqlite3
-from contextlib import closing
-from typing import List, Any
-from typing import Dict
-from typing import Tuple
+from datetime import datetime
+from typing import Dict, List
 
-from ETL import data as mock_data
+from django.core.management.base import BaseCommand
+from django.utils.timezone import make_aware
+
+from data import mock_data
+from junior_hunter.models import Company
+from junior_hunter.models import Specialty
+from junior_hunter.models import Vacancy
 
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
-
-
-class SQLiteETL:
-    def __init__(self, mock_table_name: str, sqlite_table_name: str, fields_matching: Dict):
-        self.mock_table_name = mock_table_name
-        self.sqlite_table_name = sqlite_table_name
-        self.fields_matching = fields_matching
-
-        # поля в sqlite
-        sqlite_fields_list = list(self.fields_matching.keys())
-
-        # поля в мок данных
-        self.mock_fields_list = []
-        for sqlite_field in sqlite_fields_list:
-            mock_field = self.fields_matching[sqlite_field]
-            self.mock_fields_list.append(mock_field)
-
-        # запрос для вставк данных в sqlite
-        self.query_for_insert = self._get_query_to_insert(table_name=self.sqlite_table_name,
-                                                          fields_list=sqlite_fields_list,
-                                                          )
-
-    @staticmethod
-    def _get_query_to_insert(table_name: str, fields_list: List[str]) -> str:
-        _raw_query_for_insert = """
-            INSERT INTO {table} ({fields})
-            VALUES ({fields_mask})
-        """
-
-        _fields_formatted = ",".join(fields_list)
-
-        _field_mask_list = ['?'] * len(fields_list)
-        _field_mask = ",".join(_field_mask_list)
-
-        query_for_insert = _raw_query_for_insert.format(table=table_name,
-                                                        fields=_fields_formatted,
-                                                        fields_mask=_field_mask)
-
-        return query_for_insert
-
-    def extract(self, path_to_data) -> List[Dict]:
-        """
-        [
-            {"id":1, "name": "name_1"},
-        ]
-        """
-
-        # ОСТОРОЖНО не очевидное действие
-        command_for_getting_data = f'mock_data.{self.mock_table_name}'
-        data = eval(command_for_getting_data)
-
-        return data
-
-    def transform(self, extracted_data: List[Dict]) -> Tuple[Tuple[Any]]:
-        """
-        from:
-            [..., {"id":1, "name": "name_1"}, ...]
-        to:
-            (..., (1, 'name_1'), ... )
-        """
-        data_to_send = []
-        for line in extracted_data:
-            transform_line = []
-            for field in self.mock_fields_list:
-                transform_line.append(line[field])
-            transform_line = tuple(transform_line)
-            data_to_send.append(transform_line)
-        data_to_send = tuple(data_to_send)
-
-        return data_to_send
-
-    def load(self, sqlite_conn, data_to_send: Tuple[Tuple[Any]]) -> None:
-        try:
-            cur = sqlite_conn.cursor()
-            cur.executemany(self.query_for_insert, data_to_send)
-            sqlite_conn.commit()
-        except Exception as e:
-            sqlite_conn.close()
-            raise e
 
 
 class Command(BaseCommand):
     help = 'Migrate data from mock python file, to SQLite DB'
 
-    def migration_config(self, path_to_conf: str):
-        with open(path_to_conf, 'r') as f:
-            config = yaml.safe_load(f)
-
-        return config
-
     def add_arguments(self, parser):
         parser.add_argument(
-            '--path_to_sqlite',
-            dest='path_to_sqlite',
-            help='path to sqlite DB',
-            default='db.sqlite3',
-            type=str,
+            '--skip_company',
+            dest='skip_company',
+            help='bool param allow skip company migration',
+            default=False,
+            type=bool,
         )
         parser.add_argument(
-            '--path_to_config',
-            dest='path_to_config',
-            help='path to config with declare migration',
-            type=str,
+            '--skip_specialty',
+            dest='skip_specialty',
+            help='bool param allow skip specialty migration',
+            default=False,
+            type=bool,
+        )
+        parser.add_argument(
+            '--skip_vacancy',
+            dest='skip_vacancy',
+            help='bool param allow skip vacancy migration',
+            default=False,
+            type=bool,
         )
 
     def handle(self, *args, **options):
-        # print("Hello world")
+        # company
+        if options['skip_company']:
+            logger.info('etl company skipped')
+        else:
+            logger.info('etl company start')
+            self.company_etl(data=mock_data.companies)
 
-        path_to_sqlite = options['path_to_sqlite']
-        path_to_config = options['path_to_config']
+        # specialty
+        if options['skip_specialty']:
+            logger.info('elt specialty skipped')
+        else:
+            logger.info('elt specialty start')
+            self.specialty_etl(data=mock_data.specialties)
 
-        logger.info("open connection")
-        with closing(sqlite3.connect(path_to_sqlite)) as con:
-            config = self.migration_config(path_to_config)
+        # vacancy
+        if options['skip_vacancy']:
+            logger.info('elt vacancy skipped')
+        else:
+            logger.info('etl vacancy start')
+            self.vacancy_elt(data=mock_data.jobs)
 
-            for table in config.keys():
-                logger.info(f"start migration: {table}")
+    @staticmethod
+    def company_etl(data: List[Dict]) -> None:
+        company_to_save = []
+        for company_mocked in data:
+            pk = company_mocked['id']
+            name = company_mocked['title']
+            location = company_mocked['location']
+            logo = company_mocked['logo']
+            description = company_mocked['description']
+            employee_count = company_mocked['employee_count']
 
-                logger.debug('read params from config')
-                table_conf = config[table]
-                sqlite_table_name = table_conf['table_matching']['sqlite']
-                mock_data_name = table_conf['table_matching']['mock']
-                fields_matching = table_conf['fields_matching']
+            cur_company = Company(id=pk,
+                                  name=name,
+                                  location=location,
+                                  logo=logo,
+                                  description=description,
+                                  employee_count=employee_count,
+                                  )
+            company_to_save.append(cur_company)
 
-                etl = SQLiteETL(mock_table_name=mock_data_name,
-                                sqlite_table_name=sqlite_table_name,
-                                fields_matching=fields_matching,
-                                )
-                logger.debug('extract')
-                data = etl.extract(path_to_data='data.py')
+        Company.objects.bulk_create(company_to_save)
 
-                logging.debug('transform')
-                transformed_data = etl.transform(extracted_data=data)
+        return None
 
-                logging.debug('load')
-                etl.load(sqlite_conn=con, data_to_send=transformed_data)
+    @staticmethod
+    def specialty_etl(data: List[Dict]) -> None:
+        specialty_to_save = []
+        for specialty_mocked in data:
+            code = specialty_mocked['code']
+            title = specialty_mocked['title']
 
-                logger.info('migration done!\n')
+            cur_speciality = Specialty(code=code,
+                                       title=title,
+                                       )
+            specialty_to_save.append(cur_speciality)
+
+        Specialty.objects.bulk_create(specialty_to_save)
+
+        return None
+
+    @staticmethod
+    def vacancy_elt(data: List[Dict]) -> None:
+        for job_mocked in data:
+            pk = job_mocked['id']
+            title = job_mocked['title']
+            # specialty = job_mocked['specialty']
+            # company = job_mocked['company']
+            skills = job_mocked['skills']
+            description = job_mocked['description']
+            salary_min = job_mocked['salary_from']
+            salary_max = job_mocked['salary_to']
+
+            raw_posted = job_mocked['posted']
+            raw_posted_mask = '%Y-%m-%d'
+            posted = datetime.strptime(raw_posted, raw_posted_mask)
+            posted_with_tz = make_aware(posted)
+            published_at = posted_with_tz
+
+            speciality_pk = job_mocked['specialty']
+            speciality = Specialty.objects.get(code=speciality_pk)
+
+            company_pk = int(job_mocked['company'])
+            company = Company.objects.get(id=company_pk)
+
+            vacancy = Vacancy(id=pk,
+                              title=title,
+                              specialty=speciality,
+                              skills=skills,
+                              description=description,
+                              salary_min=salary_min,
+                              salary_max=salary_max,
+                              published_at=published_at,
+                              )
+            vacancy.save()
+
+            vacancy.company.add(company)
+            vacancy.save()
+
+        return None
