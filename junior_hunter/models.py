777febb04ac9cd1128ed3edf9a020b5e363be1b0
matiiv <matiiv.r.a@gmail.com>
2022-03-10 00:22:47 +0300

    ref for flake8

--- junior_hunter/models.py
+++ junior_hunter/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.db.models import CharField
-from django.db.models import TextField
-from django.db.models import IntegerField
-from django.db.models import FloatField
 from django.db.models import DateTimeField
-from django.db.models import ManyToManyField
+from django.db.models import FloatField
 from django.db.models import ForeignKey
+from django.db.models import IntegerField
+from django.db.models import TextField
 from django.db.models import URLField
 
 
 class Company(models.Model):
+    def __str__(self):
+        return self.name
+
     name = CharField(max_length=255)
     location = CharField(max_length=255)  # TODO сделать полем с выбором, возможно из другой модели
     logo = URLField(default='https://place-hold.it/100x60')
@@ -18,12 +20,18 @@ class Company(models.Model):
 
 
 class Specialty(models.Model):
+    def __str__(self):
+        return self.title
+
     code = CharField(max_length=100, primary_key=True)  # TODO сделать полем с выбором, возможно из другой модели
     title = CharField(max_length=100)
     picture = URLField(default='https://place-hold.it/100x60')
 
 
 class Vacancy(models.Model):
+    def __str__(self):
+        return self.title
+
     title = CharField(max_length=100)
     specialty = ForeignKey(Specialty, related_name="vacancies", on_delete=models.PROTECT)
     company = ForeignKey(Company, related_name="vacancies", on_delete=models.PROTECT)
