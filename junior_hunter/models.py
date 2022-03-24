636f8fc3471235d002c0d2239193be123029f4aa
matiiv <matiiv.r.a@gmail.com>
2022-03-25 02:21:05 +0300

    change model and apply migration

--- junior_hunter/models.py
+++ junior_hunter/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import CharField
+from django.db.models import ImageField
 from django.db.models import OneToOneField
 from django.db.models import DateTimeField
 from django.db.models import ForeignKey
@@ -8,32 +9,38 @@ from django.db.models import IntegerField
 from django.db.models import TextField
 from django.db.models import URLField
 
+from config.settings import MEDIA_COMPANY_IMAGE_DIR
+from config.settings import MEDIA_SPECIALITY_IMAGE_DIR
+
 
 class Company(models.Model):
-    def __str__(self):
-        return self.name
-
+    """Компания"""
     name = CharField(max_length=255)
     location = CharField(max_length=255)  # TODO сделать полем с выбором, возможно из другой модели
-    logo = URLField(default='https://place-hold.it/100x60')
+    logo = ImageField(upload_to=MEDIA_COMPANY_IMAGE_DIR)
     description = TextField()
     employee_count = IntegerField()
     owner = OneToOneField(User, null=True, on_delete=models.PROTECT)
 
+    class Meta:
+        verbose_name_plural = 'companies'
+
+    def __str__(self):
+        return self.name
+
 
 class Specialty(models.Model):
-    def __str__(self):
-        return self.title
-
+    """Специальность"""
     code = CharField(max_length=100, primary_key=True)  # TODO сделать полем с выбором, возможно из другой модели
     title = CharField(max_length=100)
-    picture = URLField(default='https://place-hold.it/100x60')
+    picture = ImageField(upload_to=MEDIA_SPECIALITY_IMAGE_DIR)
+
+    def __str__(self):
+        return self.title
 
 
 class Vacancy(models.Model):
-    def __str__(self):
-        return self.title
-
+    """Вакансия"""
     title = CharField(max_length=100)
     specialty = ForeignKey(Specialty, related_name='vacancies', on_delete=models.PROTECT)
     company = ForeignKey(Company, related_name='vacancies', on_delete=models.PROTECT)
@@ -42,6 +49,12 @@ class Vacancy(models.Model):
     salary_min = IntegerField()
     salary_max = IntegerField()
     published_at = DateTimeField()
+
+    class Meta:
+        verbose_name_plural = 'vacancies'
+
+    def __str__(self):
+        return self.title
 
 
 class Application(models.Model):
