7dcd262bfd1685ecfcb92c06096678a93fa6ca1b
matiiv <matiiv.r.a@gmail.com>
2022-03-25 02:21:40 +0300

    add model to admin

--- junior_hunter/admin.py
+++ junior_hunter/admin.py
@@ -1,1 +1,20 @@
-# Register your models here.
+from django.contrib import admin
+
+from junior_hunter.models import Application
+from junior_hunter.models import Company
+from junior_hunter.models import Vacancy
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    pass
